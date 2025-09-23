9872380670c64db1e4717ed211cd2eb231375c43
Eudjean961 <e.boganov777@gmail.com>
2025-09-23 12:31:44 +0500

    feat(common): add zakupai_common lib and CI integration

---
+++ libs/zakupai_common/zakupai_common/logging.py
@@ -0,0 +1,12 @@
+import logging
+
+
+def setup_logging(service_name: str):
+    logger = logging.getLogger(service_name)
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    handler.setFormatter(
+        logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    )
+    logger.addHandler(handler)
+    return logger
